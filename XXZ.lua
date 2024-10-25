1cc1f4b6009c5effc83c9cdc9f31501909bdd2d5
binjiaob <1549528531@qq.com>
2024-10-25 17:58:55 +0800

    Add files via upload

--- XXZ.lua
+++ XXZ.lua
@@ -286,20 +286,7 @@ Tab:AddTextbox({
                 })
 	end
 })
-Tab:AddTextbox({
-	Name = "跳跃高度设置",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-		
-		OrionLib:MakeNotification({
-                    Name = "成功设置跳跃高度:  "..game.Players.LocalPlayer.Character.Humanoid.WalkSpeed.."   |高度",
-                    Content = "成功",
-                    Time = 3
-                })
-	end
-})
+
 Tab:AddTextbox({
     Name = '范围可随便调',
     Default = '',
