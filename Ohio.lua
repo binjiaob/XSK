238d4057cd632895abb6c13e25dc4efb83577cb0
binjiaob <1549528531@qq.com>
2024-10-27 07:14:03 +0800

    Add files via upload

---
+++ Ohio.lua
@@ -0,0 +1,162 @@
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/BINjiaobzx6/BINjiao/refs/heads/main/Vape.txt")() 
+
+local OpenUI = Instance.new("ScreenGui") 
+local ImageButton = Instance.new("ImageButton") 
+local UICorner = Instance.new("UICorner") 
+OpenUI.Name = "OpenUI" 
+OpenUI.Parent = game.CoreGui 
+OpenUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling 
+ImageButton.Parent = OpenUI 
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
+ImageButton.BackgroundTransparency = 0.500 
+ImageButton.Position = UDim2.new(0.0235790554, 0, 0.466334164, 0) 
+ImageButton.Size = UDim2.new(0, 50, 0, 50) 
+ImageButton.Image = "rbxassetid://117494350103589" 
+ImageButton.Draggable = true 
+UICorner.CornerRadius = UDim.new(0, 200) 
+UICorner.Parent = ImageButton 
+ImageButton.MouseButton1Click:Connect(function() 
+  if uihide == false then
+	uihide = true
+	game.CoreGui.ui.Main:TweenSize(UDim2.new(0, 0, 0, 0),"In","Quad",0.4,true)
+else
+	uihide = false
+	game.CoreGui.ui.Main:TweenSize(UDim2.new(0, 560, 0, 319),"Out","Quad",0.4,true)
+		end 
+		
+end)
+
+uihide = false
+
+ local win = lib:Window("XKHUB 丨 ohio ",Color3.fromRGB(255, 24, 24), Enum.KeyCode.RightControl) 
+  
+ local setup = win:Tab("其余")
+
+ setup:Label("作者：XUAN_RT")
+ 
+setup:Button("移除声音",function()
+for i, v in pairs(game.Workspace["_WorldOrigin"]:GetChildren()) do
+if v.Name == "CurvedRing" or v.Name == "SlashHit" or v.Name == "SwordSlash" or v.Name == "Sounds" then
+v:Destroy()
+end
+end
+end)
+setup:Colorpicker("更改用户界面颜色",Color3.fromRGB(44, 120, 224), function(t) 
+ lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255)) 
+ end)
+ 
+ 
+ local tab = win:Tab("自动化")
+
+tab:Toggle("收集物品现金", false, function(v)
+    ItemFarm = v
+        
+        if ItemFarm then
+            pcall(function()
+                ItemFarmFunc()
+            end)
+        end
+end)
+
+function GetItems()
+   local cache = {}
+   
+   for i,v in pairs(game:GetService("Workspace").Game.Entities.CashBundle:GetChildren()) do
+       table.insert(cache,v)
+   end
+   
+   for i,v in pairs(game:GetService("Workspace").Game.Entities.ItemPickup:GetChildren()) do
+       table.insert(cache,v)
+   end
+   
+   return cache
+end
+
+function Collect(item)
+    if item:FindFirstChildOfClass("ClickDetector") then
+        fireclickdetector(item:FindFirstChildOfClass("ClickDetector"))
+    elseif item:FindFirstChildOfClass("Part") then
+        local maincrap = item:FindFirstChildOfClass("Part")
+        fireclickdetector(maincrap:FindFirstChildOfClass("ClickDetector"))
+    end
+end
+
+ItemFarmFunc = function()
+    while ItemFarm and task.wait() do
+        local allitems = GetItems()
+        
+        for i,v in pairs(allitems) do
+            if ItemFarm == false then break end
+            pcall(function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v:FindFirstChildOfClass("Part").CFrame
+                task.wait(0.5)
+                Collect(v)
+                task.wait(0.5)
+            end)
+            continue
+        end
+    end
+end
+
+tab:Toggle("自动抢银行", false, function(v)
+    AutoRobBank = v
+        
+        if AutoRobBank then
+            pcall(function()
+                AutoRobBankFunc()
+            end)
+        end
+end)
+
+AutoRobBankFunc = function()
+    while AutoRobBank and task.wait() do
+        local bankthing = game:GetService("Workspace").BankRobbery.BankCash
+        if #bankthing.Cash:GetChildren() > 0 then
+           game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = bankthing.Main.CFrame
+           task.wait()
+           fireproximityprompt(game:GetService("Workspace").BankRobbery.BankCash.Main.Attachment.ProximityPrompt)
+        end
+    end
+end
+
+Player:Dropdown Options = {'银行','珠宝店','沙滩','武器店（撬锁）','武士刀','射线枪','加特林','锯掉','沙漠之鹰','警察局（M4A1）','AUG','军事基地（军甲）'},
+    Callback = function(Value)
+    local epoh2 = game:GetService('Players')
+    local epoh3 = epoh2.LocalPlayer.Character.HumanoidRootPart
+    if Value == '银行' then
+        local epoh1 = CFrame.new(1055.94153, 15.11950874, -344.58374)
+        epoh3.CFrame = epoh1
+    elseif Value == '珠宝店' then
+        local epoh1 = CFrame.new(1719.02637, 14.2831011, -714.293091)
+        epoh3.CFrame = epoh1
+    elseif Value == '沙滩' then
+        local epoh1 = CFrame.new(998.4656372070312, 15, 395.9789733886719)
+        epoh3.CFrame = epoh1
+    elseif Value == '武器店（撬锁）' then
+        local epoh1 = CFrame.new(660.5284423828125, 6.4081127643585205, -716.489990234375)
+        epoh3.CFrame = epoh1
+    elseif Value == '武士刀' then
+        local epoh1 = CFrame.new(175.191, 13.937, -132.69)
+        epoh3.CFrame = epoh1
+    elseif Value == '射线枪' then
+        local epoh1 = CFrame.new(148.685471, -90, -529.280945)
+        epoh3.CFrame = epoh1
+    elseif Value == '加特林' then
+        local epoh1 = CFrame.new(364.97076416015625, 0.764974117279053, -1447.3302001953125)
+        epoh3.CFrame = epoh1
+    elseif Value == '锯掉' then
+        local epoh1 = CFrame.new(1179.98523,40,-436.812683)
+        epoh3.CFrame = epoh1
+    elseif Value == '沙漠之鹰' then
+        local epoh1 = CFrame.new(363.341461, 26.0798492, -259.681396)
+        epoh3.CFrame = epoh1
+    elseif Value == '警察局（M4A1）' then
+        local epoh1 = CFrame.new(603.4676513671875,25.662811279296875,-922.0442504882812)
+        epoh3.CFrame = epoh1
+    elseif Value == 'AUG' then
+        local epoh1 = CFrame.new(1170.500244140625,48.37138366699219,-772.55859375)
+        epoh3.CFrame = epoh1
+    elseif Value == '军事基地（军甲）' then
+        local epoh1 = CFrame.new(563.4422607421875,28.502071380615234,-1472.780517578125)
+        epoh3.CFrame = epoh1
+    end
