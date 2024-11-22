cc7ab1a9a4022d8ea271f1a2c5179ec3d579e7fd
binjiaob <1549528531@qq.com>
2024-11-22 23:04:06 +0800

    Add files via upload

--- XK脚本中心V3.lua
+++ XK脚本中心V3.lua
@@ -5872,79 +5872,6 @@ end)
 about:Toggle("黑洞脚本", "ToggleInfo", false, function(bool)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/binjiaob/XSK/refs/heads/main/XK%E8%84%9A%E6%9C%AC%E4%B8%AD%E5%BF%83V3.lua"))()
 end)
-
-local creds = window:Tab("忍者元素大亨", "6035145364")
-    local about = creds:section("通用内容", true)
-    
-    about:Toggle("箱子透视","Valkiry",false,function(state)
-        if state then
-            _G.Tree2ESPInstances = {}
-            local esptable = {doors = {}}
-
-            local function createBillboard(instance, name, color)
-                local bill = Instance.new("BillboardGui", game.CoreGui)
-                bill.AlwaysOnTop = true
-                bill.Size = UDim2.new(0, 100, 0, 50)
-                bill.Adornee = instance
-                bill.MaxDistance = 2000
-
-                local mid = Instance.new("Frame", bill)
-                mid.AnchorPoint = Vector2.new(0.5, 0.5)
-                mid.BackgroundColor3 = color
-                mid.Size = UDim2.new(0, 8, 0, 8)
-                mid.Position = UDim2.new(0.5, 0, 0.5, 0)
-                Instance.new("UICorner", mid).CornerRadius = UDim.new(1, 0)
-                Instance.new("UIStroke", mid)
-
-                local txt = Instance.new("TextLabel", bill)
-                txt.AnchorPoint = Vector2.new(0.5, 0.5)
-                txt.BackgroundTransparency = 1
-                txt.TextColor3 = color
-                txt.Size = UDim2.new(1, 0, 0, 20)
-                txt.Position = UDim2.new(0.5, 0, 0.7, 0)
-                txt.Text = name
-                Instance.new("UIStroke", txt)
-
-                task.spawn(function()
-                    while bill do
-                        if bill.Adornee == nil or not bill.Adornee:IsDescendantOf(workspace) then
-                            bill.Enabled = false
-                            bill.Adornee = nil
-                            bill:Destroy()
-                        end
-                        task.wait()
-                    end
-                end)
-            end
-
-            local function monitorTree2()
-                for _, instance in pairs(workspace:GetDescendants()) do
-                    if instance:IsA("Model") and instance.Name == "BalloonCrate" then
-                        createBillboard(instance, "箱子", Color3.new(255, 0, 0)) -- Change color as needed
-                    end
-                end
-
-                workspace.DescendantAdded:Connect(function(instance)
-                    if instance:IsA("Model") and instance.Name == "BalloonCrate" then
-                        createBillboard(instance, "箱子", Color3.new(255, 0, 0)) -- Change color as needed
-                    end
-                end)
-            end
-
-            monitorTree2()
-            table.insert(_G.Tree2ESPInstances, esptable)
-				
-        else
-            if _G.Tree2ESPInstances then
-                for _, instance in pairs(_G.Tree2ESPInstances) do
-                    for _, v in pairs(instance.doors) do
-                        v.delete()
-                    end
-                end
-                _G.Tree2ESPInstances = nil
-            end
-        end
-    end)
     
     local creds = window:Tab("压力", "6035145364")
     local about = creds:section("通用内容", true)
@@ -6972,22 +6899,6 @@ local creds = window:Tab("忍者元素大亨", "6035145364")
                     end
                 end
                 _G.nahInstances = nil
-            end
-        end
-    end)
-    
-    about:Toggle("飞行脚本","Valkiry",false,function(state)
-    isFlying = val
-        if isFlying then
-            -- Start flat flying thread
-            flyThread = coroutine.wrap(function()
-                flatFly(2)  -- Default flat fly speed
-            end)
-            flyThread()
-        else
-            -- Stop flat flying
-            if flyThread then
-                flyThread = nil
             end
         end
     end)
